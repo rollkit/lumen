e46e46b5342c312c2079a993182c60eb7f06e533
tac0turtle <marko@baricevic.me>
2025-07-10 16:53:12 +0200

    pull out validator

--- bin/lumen/src/main.rs
+++ bin/lumen/src/main.rs
@@ -8,6 +8,7 @@
 pub mod attributes;
 pub mod builder;
 pub mod error;
+pub mod validator;
 
 use alloy_rpc_types::engine::{
     ExecutionData, ExecutionPayloadEnvelopeV2, ExecutionPayloadEnvelopeV3,
@@ -17,15 +18,10 @@ use clap::Parser;
 use reth_ethereum::{
     chainspec::ChainSpec,
     node::{
-        api::{
-            payload::{EngineApiMessageVersion, EngineObjectValidationError, PayloadOrAttributes},
-            validate_version_specific_fields, AddOnsContext, EngineTypes, EngineValidator,
-            FullNodeComponents, FullNodeTypes, InvalidPayloadAttributesError, NewPayloadError,
-            NodeTypes, PayloadTypes, PayloadValidator,
-        },
+        api::{EngineTypes, FullNodeTypes, NodeTypes, PayloadTypes},
         builder::{
             components::{BasicPayloadServiceBuilder, ComponentsBuilder},
-            rpc::{EngineValidatorBuilder, RpcAddOns},
+            rpc::RpcAddOns,
             Node, NodeAdapter, NodeComponentsBuilder,
         },
         node::{
@@ -34,19 +30,18 @@ use reth_ethereum::{
         },
         EthereumEthApiBuilder,
     },
-    primitives::{RecoveredBlock, SealedBlock},
+    primitives::SealedBlock,
 };
 use reth_ethereum_cli::{chainspec::EthereumChainSpecParser, Cli};
-use reth_ethereum_payload_builder::EthereumExecutionPayloadValidator;
 use reth_payload_builder::EthBuiltPayload;
 use reth_trie_db::MerklePatriciaTrie;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
 use tracing::info;
 
 use crate::{
     attributes::{RollkitEnginePayloadAttributes, RollkitEnginePayloadBuilderAttributes},
     builder::{RollkitArgs, RollkitPayloadBuilderBuilder},
+    validator::RollkitEngineValidatorBuilder,
 };
 
 #[global_allocator]
@@ -83,139 +78,6 @@ impl EngineTypes for RollkitEngineTypes {
     type ExecutionPayloadEnvelopeV3 = ExecutionPayloadEnvelopeV3;
     type ExecutionPayloadEnvelopeV4 = ExecutionPayloadEnvelopeV4;
     type ExecutionPayloadEnvelopeV5 = ExecutionPayloadEnvelopeV5;
-}
-
-/// Rollkit engine validator that handles custom payload validation
-#[derive(Debug, Clone)]
-pub struct RollkitEngineValidator {
-    inner: EthereumExecutionPayloadValidator<ChainSpec>,
-}
-
-impl RollkitEngineValidator {
-    /// Instantiates a new validator.
-    pub const fn new(chain_spec: Arc<ChainSpec>) -> Self {
-        Self {
-            inner: EthereumExecutionPayloadValidator::new(chain_spec),
-        }
-    }
-
-    /// Returns the chain spec used by the validator.
-    #[inline]
-    fn chain_spec(&self) -> &ChainSpec {
-        self.inner.chain_spec().as_ref()
-    }
-}
-
-impl PayloadValidator for RollkitEngineValidator {
-    type Block = reth_ethereum::Block;
-    type ExecutionData = ExecutionData;
-
-    fn ensure_well_formed_payload(
-        &self,
-        payload: ExecutionData,
-    ) -> Result<RecoveredBlock<Self::Block>, NewPayloadError> {
-        info!("Rollkit engine validator: validating payload");
-
-        // Use inner validator but with custom rollkit handling
-        match self.inner.ensure_well_formed_payload(payload.clone()) {
-            Ok(sealed_block) => {
-                info!("Rollkit engine validator: payload validation succeeded");
-                sealed_block
-                    .try_recover()
-                    .map_err(|e| NewPayloadError::Other(e.into()))
-            }
-            Err(err) => {
-                // Log the error for debugging
-                tracing::debug!("Rollkit payload validation error: {:?}", err);
-
-                // Check if this is a block hash mismatch error - bypass it for rollkit
-                if matches!(err, alloy_rpc_types::engine::PayloadError::BlockHash { .. }) {
-                    info!("Rollkit engine validator: bypassing block hash mismatch for rollkit");
-                    // For rollkit, we trust the payload builder - just parse the block without hash validation
-                    use reth_primitives_traits::Block;
-                    let ExecutionData { payload, sidecar } = payload;
-                    let sealed_block = payload.try_into_block_with_sidecar(&sidecar)?.seal_slow();
-                    sealed_block
-                        .try_recover()
-                        .map_err(|e| NewPayloadError::Other(e.into()))
-                } else {
-                    // For other errors, re-throw them
-                    Err(NewPayloadError::Eth(err))
-                }
-            }
-        }
-    }
-}
-
-impl<T> EngineValidator<T> for RollkitEngineValidator
-where
-    T: PayloadTypes<
-        PayloadAttributes = RollkitEnginePayloadAttributes,
-        ExecutionData = ExecutionData,
-    >,
-{
-    fn validate_version_specific_fields(
-        &self,
-        version: EngineApiMessageVersion,
-        payload_or_attrs: PayloadOrAttributes<'_, Self::ExecutionData, T::PayloadAttributes>,
-    ) -> Result<(), EngineObjectValidationError> {
-        validate_version_specific_fields(self.chain_spec(), version, payload_or_attrs)
-    }
-
-    fn ensure_well_formed_attributes(
-        &self,
-        version: EngineApiMessageVersion,
-        attributes: &T::PayloadAttributes,
-    ) -> Result<(), EngineObjectValidationError> {
-        validate_version_specific_fields(
-            self.chain_spec(),
-            version,
-            PayloadOrAttributes::<Self::ExecutionData, T::PayloadAttributes>::PayloadAttributes(
-                attributes,
-            ),
-        )?;
-
-        // Validate rollkit-specific attributes
-        if let Some(ref transactions) = attributes.transactions {
-            info!(
-                "Rollkit engine validator: validating {} transactions",
-                transactions.len()
-            );
-        }
-
-        Ok(())
-    }
-
-    fn validate_payload_attributes_against_header(
-        &self,
-        _attr: &<T as PayloadTypes>::PayloadAttributes,
-        _header: &<Self::Block as reth_ethereum::primitives::Block>::Header,
-    ) -> Result<(), InvalidPayloadAttributesError> {
-        // Skip default timestamp validation for rollkit
-        Ok(())
-    }
-}
-
-/// Rollkit engine validator builder
-#[derive(Debug, Default, Clone, Copy)]
-#[non_exhaustive]
-pub struct RollkitEngineValidatorBuilder;
-
-impl<N> EngineValidatorBuilder<N> for RollkitEngineValidatorBuilder
-where
-    N: FullNodeComponents<
-        Types: NodeTypes<
-            Payload = RollkitEngineTypes,
-            ChainSpec = ChainSpec,
-            Primitives = reth_ethereum::EthPrimitives,
-        >,
-    >,
-{
-    type Validator = RollkitEngineValidator;
-
-    async fn build(self, ctx: &AddOnsContext<'_, N>) -> eyre::Result<Self::Validator> {
-        Ok(RollkitEngineValidator::new(ctx.config.chain.clone()))
-    }
 }
 
 /// Rollkit node type
